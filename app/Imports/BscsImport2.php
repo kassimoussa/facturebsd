69d2c7508374becba587179af64b40f3c21970c5
kassimoussa <kassim.moussali@gmail.com>
2023-02-01 16:06:39 +0300

    vf

--- app/Imports/BscsImport2.php
+++ app/Imports/BscsImport2.php
@@ -7,19 +7,20 @@ use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class BscsImport2 implements ToCollection
-{
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+class BscsImport2 implements ToCollection, WithChunkReading
+{ 
     public function collection(Collection $rows)
     {
         foreach ($rows as $row) 
         {
             Bscs::where('name', @$row[0])->delete();
         }
+    }
+
+    public function chunkSize(): int
+    {
+        return 100;
     }
 }
